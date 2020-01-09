2aff7915367ea18dfaf8f16331a6274a94a11151
PianoFF <Fangfei1star@gmail.com>
2020-01-09 16:19:46 +0000

    almost working filter

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -2,11 +2,13 @@ import React from 'react'
 
 class Filters extends React.Component {
   render() {
+    const {onChange} = this.props 
+
     return (
       <div className="ui form">
         <h3>Animal type</h3>
         <div className="field">
-          <select name="type" id="type">
+          <select name="type" id="type" onChange={(event)=>{onChange(event)}}>
             <option value="all">All</option>
             <option value="cat">Cats</option>
             <option value="dog">Dogs</option>
